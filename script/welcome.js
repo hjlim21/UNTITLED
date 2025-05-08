54c24759fe1eb9e860937b7197347e3521da4d91
hjlim21 <162293576+hjlim21@users.noreply.github.com>
2025-05-08 21:57:19 +0900

    인간버튼

--- script/welcome.js
+++ script/welcome.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     messageBox.textContent = '상명대학교 커뮤니케이션디자인전공 과제전';
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const intBtn = document.querySelector('.btn.visite');
+  const messageBox = document.getElementById('message');
+
+  intBtn.addEventListener('click', () => {
+    messageBox.textContent =
+      '과제전에 오신 여러분들 하이염 저희의 관람 포인트는 응애응애 입니다';
+  });
+});
