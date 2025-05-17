a673e76d88db76d780f9df5d274af3f6c4f518cf
hjlim21 <162293576+hjlim21@users.noreply.github.com>
2025-05-17 19:39:47 +0900

    제발

--- script/index.js
+++ script/index.js
@@ -1,15 +1,34 @@
+// $(document).ready(function () {
+//   const texts = ['UNTITLED', 'FFF'];
+//   let index = 0;
+
+//   $('#switchTextBtn').click(function () {
+//     const currentText = $(this).find('span').text();
+
+//     if (currentText === 'FFF') {
+//       alert(
+//         '이름 붙일 수 없는 시도들, 정의하기 이전의 생각들, 디자인의 기록입니다.'
+//       );
+//       window.location.href = 'welcome.html';
+//       return;
+//     }
+
+//     index = (index + 1) % texts.length;
+//     $(this).find('span').text(texts[index]);
+//   });
+// });
 $(document).ready(function () {
   const texts = ['UNTITLED', 'FFF'];
   let index = 0;
 
   $('#switchTextBtn').click(function () {
-    const currentText = $(this).find('span').text();
+    const currentText = $(this).find('span').text().trim();
 
     if (currentText === 'FFF') {
       alert(
         '이름 붙일 수 없는 시도들, 정의하기 이전의 생각들, 디자인의 기록입니다.'
       );
-      window.location.href = 'welcome.html';
+      window.location.href = 'welcome.html'; // 경로 확인 필수!
       return;
     }
 
