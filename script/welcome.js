9ebb5254b9cfa0f80f4142c5d56bd9c06f6ba87f
hjlim21 <162293576+hjlim21@users.noreply.github.com>
2025-05-19 20:23:28 +0900

    gif

--- script/welcome.js
+++ script/welcome.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   intBtn.addEventListener("click", () => {
     messageBox.innerHTML =  `
     <p>커뮤니케이션디자인 전공의 다양한 수업에서 시작된 과제들을 모았습니다.</p>
-    <p>이름 붙일 수 없는 시도와 고민의 결과물들이 한자리에 모인, 되짚어볼 수 있도록 만든 디자인들의 기록입니다.</p>
+    <p>이름 붙일 수 없는 시도와 고민의 결과물들이 한자리에 모인, 되짚어볼 수 있는 기록입니다.</p>
     <p>전시장 안팎에서, 커디 학생들의 생각과 시도를 자유롭게 감상해보세요.</p>
   `;
   });
